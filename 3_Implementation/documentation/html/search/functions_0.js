4cb47b5461a141672c5ce33ce7003171e476b759
Pavankumar.R <102242702+Pavankumar1719@users.noreply.github.com>
2022-04-02 20:14:12 +0530

    Delete

--- 3_Implementation/documentation/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cancelticket_0',['cancelticket',['../function_8h.html#ac7c9598feced0892b77d243eae12f8e9',1,'function.h']]],
-  ['changepriceticket_1',['changepriceticket',['../function_8h.html#a73642167e72a0d47c00dc8849d9623e7',1,'function.h']]]
-];
